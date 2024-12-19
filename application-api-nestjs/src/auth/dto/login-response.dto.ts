d939c0a54b1433f5bc6f34eccf1bab5e4ee1d6e6
Mohamed Mousa <mohamedamousa00@gmail.com>
2024-12-19 09:52:28 +0200

    remove not working code

--- application-api-nestjs/src/auth/dto/login-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class LoginResponseDto {
-  @ApiProperty({
-    description: 'JWT token for authentication',
-    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
-  })
-  token: string;
-}
